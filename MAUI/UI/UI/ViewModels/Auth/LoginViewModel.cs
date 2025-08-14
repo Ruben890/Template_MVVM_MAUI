17382dcc565cc6d6cdc9bac969b308022f1d3e45
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-08-14 16:33:22 -0400

    Refactor solution structure and enhance login features
    - Removed "Appliaction" project; added "Shared" and "Infrastructure" projects in `MAUI.sln`.
    - Updated `Components.csproj` to exclude certain resources and compile items.
    - Removed "Domain" project reference from `UI.csproj`.
    - Enhanced `LoginViewModel.cs` with new properties and commands for user authentication.
    - Redesigned `LoginPage.xaml` for improved user interface, including input fields and a login button.

--- MAUI/UI/UI/ViewModels/Auth/LoginViewModel.cs
+++ MAUI/UI/UI/ViewModels/Auth/LoginViewModel.cs
@@ -1,4 +1,10 @@
 ﻿using Appliaction.Interfaces.IServices;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using Shared.Enums;
+using Shared.Models.Auth;
+using System.Reflection;
+using System.Windows.Input;
 
 namespace UI.ViewModels.Auth
 {
@@ -6,10 +12,60 @@ namespace UI.ViewModels.Auth
     {
         private readonly IAuthServices _authServices;
 
-        public LoginViewModel(IAuthServices authServices)
+        private string _email = string.Empty;
+        private string _password = string.Empty;
+        private bool _isSyncing = false;
+        private string _syncStatus = string.Empty;
+        private string _syncName = string.Empty;
+
+        public string Email
         {
-            _authServices = authServices ?? throw new ArgumentNullException(nameof(authServices));
+            get => _email;
+            set { _email = value; OnPropertyChanged(); }
         }
 
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand ForgotPasswordCommand { get; }
+
+        public string AppVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "v1.0.0";
+
+        public LoginViewModel(IAuthServices authServices)
+        {
+            _authServices = authServices;
+
+            LoginCommand = new Command(async () => await LoginAsync());
+            ForgotPasswordCommand = new Command(async () => await ForgotPasswordAsync());
+        }
+
+        private async Task LoginAsync()
+        {
+            if (_authServices == null) return;
+
+            var request = new AuthLoginRequest
+            {
+                Email = this.Email,
+                Password = this.Password,
+            };
+
+            var result = await _authServices.Login(request);
+
+            if (result.Status == ServiceResultStatus.Fail || result.Status == ServiceResultStatus.Warning)
+            {
+                await Toast.Make(result.Message!, ToastDuration.Long, 14).Show();
+                return;
+            }
+
+        }
+
+        private async Task ForgotPasswordAsync()
+        {
+            await Application.Current!.MainPage!.DisplayAlert("Recuperar contraseña", "Aquí iría la lógica", "OK");
+        }
     }
 }
