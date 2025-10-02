b66245f7c71b0c9431421ee2123b984bb6fdadcf
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-10-02 11:42:55 -0400

    se agrego fontAwsome

--- MAUI/UI/UI/Extencions/ServiceCollectionExtensions.cs
+++ MAUI/UI/UI/Extencions/ServiceCollectionExtensions.cs
@@ -5,14 +5,23 @@ namespace UI.Config
 {
     public static class ServiceCollectionExtensions
     {
-        /// <summary>
-        /// Registra todas las páginas (views) y sus ViewModels en el contenedor de servicios.
-        /// Utiliza AddTransient para crear una nueva instancia cada vez que se solicite.
-        /// </summary>
-        public static void AddViewsAndViewModels(this IServiceCollection services)
+        public static void AddPagesAndViewModels(this IServiceCollection services)
         {
-            services.AddTransient<LoginViewModel>();
+            services.AddViewModels();
+            services.AddViews();
+        }
+
+
+        public static void AddViews(this IServiceCollection services)
+        {
             services.AddTransient<LoginPage>();
+        }
+
+        public static void AddViewModels(this IServiceCollection services)
+        {
+            // Registro de páginas (views)
+            services.AddTransient<LoginViewModel>();
+            services.AddViews();
         }
     }
 }
